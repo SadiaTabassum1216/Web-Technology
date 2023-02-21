797afb5a38076f734fbac1ab445f50617549bc81
Sadia Tabassum <60074200+SadiaTabassum1216@users.noreply.github.com>
2023-02-22 00:53:32 +0600

    Add files via upload

---
+++ form using PHP/submit.php
@@ -0,0 +1,48 @@
+<html>
+  <head>
+    <title>submitted value</title>
+    <style>
+      body{
+        font-size:larger;
+        background-color: #d3e0e4;
+        padding: 80px;
+      }
+
+      img{
+        max-height: 300px;
+      }
+    
+      #left{   
+        float: left;
+      }
+
+      #right{
+        float: right;
+      }
+    </style>
+
+  </head>
+<body>
+<div id="left">
+<?php
+  $fname = $_POST["fname"];
+  $lname = $_POST["lname"];
+  $email = $_POST["email"];
+  $address = $_POST["address"];
+  $photo = $_FILES["photo"]["name"];
+
+  echo "<p>Name: ${fname} ${lname}</p>";
+  echo "<p>Email: ${email}</p>";
+  echo "<p>Address: ${address}</p>";
+  ?> 
+</div>
+
+
+ <div id="right">
+ <?php 
+ echo '<img src="'.$photo.'" alt="Image">';
+ ?>
+ </div>
+
+</body>
+</html>
